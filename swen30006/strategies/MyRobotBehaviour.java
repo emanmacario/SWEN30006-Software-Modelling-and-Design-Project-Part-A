b28ec8d0d41fd8cc631e391f8884307794eb9ee4
EmanMacario <emacario1997@hotmail.com>
2018-03-09 12:45:54 +1100

    Added MyMailPool and MyRobotBehaviour classes to strategies package

---
+++ swen30006/strategies/MyRobotBehaviour.java
@@ -0,0 +1,23 @@
+package strategies;
+
+import automail.StorageTube;
+
+public class MyRobotBehaviour implements IRobotBehaviour {
+	
+	@Override
+	public void startDelivery() {
+		// TO DO
+	}
+	
+	@Override
+	public boolean returnToMailRoom(StorageTube tube) {
+		// TO DO
+		return false;
+	}
+	
+	@Override
+	public void priorityArrival(int priority, int weight) {
+		// TO DO
+	}
+}
+
