6c69ed6159868df194b00ad4eb9396271bd3d970
EmanMacario <emacario1997@hotmail.com>
2018-03-10 17:56:58 +1100

    Commented interface functions in MyMailPool and MyRobotBehaviour classes

--- swen30006/strategies/MyMailPool.java
+++ swen30006/strategies/MyMailPool.java
@@ -1,16 +1,33 @@
 package strategies;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+
 import automail.MailItem;
 import automail.StorageTube;
 
 public class MyMailPool implements IMailPool {
-
+	
+	// Idea: use a priority queue.
+	// The comparator needs to prioritise based on 3 factors: floor, weight, priority
+	
+	
+	
+	/**
+     * Adds an item to the mail pool
+     * @param mailItem the mail item being added.
+     */
 	@Override
 	public void addToPool(MailItem mailItem) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	/**
+     * @param tube refers to the pack the robot uses to deliver mail.
+     * @param strong is whether the tube belongs to a strong robot.
+     */
 	@Override
 	public void fillStorageTube(StorageTube tube, boolean strong) {
 		// TODO Auto-generated method stub
--- swen30006/strategies/MyRobotBehaviour.java
+++ swen30006/strategies/MyRobotBehaviour.java
@@ -4,17 +4,36 @@ import automail.StorageTube;
 
 public class MyRobotBehaviour implements IRobotBehaviour {
 	
+	/**
+	 * startDelivery() provides the robot the opportunity to 
+	 * initialise state in support of the other methods below. 
+	 */
 	@Override
 	public void startDelivery() {
 		// TO DO
 	}
 	
+	
+	/** 
+	 * @param tube refers to the pack the robot uses to deliver mail.
+	 * @return When this is true, the robot is returned to the mail room.
+	 * The robot will always return to the mail room when the tube is empty.
+	 * This method allows the robot to return with items still in the tube,
+	 * if circumstances make this desirable.
+	 */
 	@Override
 	public boolean returnToMailRoom(StorageTube tube) {
 		// TO DO
 		return false;
 	}
 	
+	
+	/**
+     * @param priority is that of the priority mail item which just arrived.
+     * @param weight is that of the same item.
+     * The automail system broadcasts this information to all robots
+     * when a new priority mail items arrives at the building.
+     */
 	@Override
 	public void priorityArrival(int priority, int weight) {
 		// TO DO
